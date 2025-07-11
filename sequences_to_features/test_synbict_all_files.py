f93deaa130422008f71ec6ce306c69642c222ea6
cl117 <cl117@rice.edu>
2025-07-10 18:51:22 -0600

    test synbict

--- sequences_to_features/test_synbict_all_files.py
+++ sequences_to_features/test_synbict_all_files.py
@@ -5,7 +5,8 @@ from collections import Counter
 from rdflib import URIRef
 DCTERMS_TITLE = URIRef('http://purl.org/dc/terms/title')
 
-input_file = sys.argv[1]
+dir = "/home/sophia/git_repo/Addgene-Annotation"
+addgene_sbol_list = os.listdir(os.path.join(dir, 'addgene_sbol'))[0:100]
 
 def get_feature_libraries_paths(feature_libraries_dir) -> str:
     # Get the paths of all feature library files in the feature_libraries_dir
@@ -24,7 +25,7 @@ def get_feature_libraries():
     return feature_library
 
 #@profile
-def test_annotate(feature_library, doc):
+def test_annotate(feature_library, doc): 
     output_docs = []
     output_library = FeatureLibrary(output_docs, False)
     # doc is target_doc
@@ -37,38 +38,50 @@ def get_title_from_sbol(f2):
     for cd in f2.componentDefinitions:
         for sa in cd.sequenceAnnotations:
             if DCTERMS_TITLE in sa.properties:
-                title = sa.properties[DCTERMS_TITLE][0]
-                titles.append(str(title))
+                if(len(sa.properties[DCTERMS_TITLE]) > 0):
+                    title = sa.properties[DCTERMS_TITLE][0]
+                    titles.append(str(title))
             else:
                 print("dcterms:title not found")
     return titles
 
-doc = load_sbol(input_file) 
 feature_library = get_feature_libraries()
-titles1 = get_title_from_sbol(doc)
-
-start = time.perf_counter()
-tracemalloc.start()
-
-test_annotate(feature_library, doc)
-
-current, peak = tracemalloc.get_traced_memory()
-tracemalloc.stop()
-end = time.perf_counter()
-elapsed_ms = (end - start) * 1000
-titles2 = get_title_from_sbol(doc)
-c1 = Counter(titles1)
-c2 = Counter(titles2)
-# Subtract counts to get new elements in list2
-new_elements = list((c2 - c1).elements())
-
-output = {
-    "time": elapsed_ms,
-    "peak_memory": peak / 1024 / 1024,
-    "new_elements": new_elements,
-    "number": len(new_elements)
-}
-
-out_file = name_only = os.path.splitext(os.path.basename(input_file))[0]+".json"
+out_file = "combined_synbict_results_2.json"
 with open(out_file, 'w') as f:
-    json.dump(output, f)
+    f.write("{\n")  # Start of JSON object
+    for ind, file in enumerate(addgene_sbol_list[62:100]):
+        doc = load_sbol(os.path.join(dir, 'addgene_sbol', file))
+        titles1 = get_title_from_sbol(doc)
+
+        start = time.perf_counter()
+        tracemalloc.start()
+
+        test_annotate(feature_library, doc)
+
+        current, peak = tracemalloc.get_traced_memory()
+        peak = peak / 1024 / 1024
+        tracemalloc.stop()
+        end = time.perf_counter()
+        elapsed_ms = (end - start) * 1000
+        titles2 = get_title_from_sbol(doc)
+        c1 = Counter(titles1)
+        c2 = Counter(titles2)
+        # Subtract counts to get new elements in list2
+        new_elements = list((c2 - c1).elements())
+
+        output = {
+            "time": round(elapsed_ms, 2), 
+            "peak_memory": round(peak, 2), 
+            "new_elements": new_elements,
+            "number": len(new_elements)
+        }
+        key = os.path.basename(file)
+        json_str = json.dumps(output, indent=2)
+
+        # Add comma except for the last element
+        comma = ',' if ind < len(addgene_sbol_list) - 1 else ''
+        f.write(f'  "{key}": {json_str}{comma}\n')
+        f.flush()
+        os.fsync(f.fileno())
+
+    f.write("}\n")  # End of JSON object
